0e626c06c88f310598506f2047ffe0a188ac3a21
Trung Phan <trungphan1328@outlook.com>
2021-02-17 21:19:30 +0700

    Add work and tag done - Buttons hidden

--- src/InputForm.js
+++ src/InputForm.js
@@ -1,17 +1,32 @@
+import React from 'react';
 import { Button, Form, Input } from 'antd';
 
-export default function InputForm(props) {
-  return (
-    <div>
-      <Form.Item>
-        <Input
-          placeholder="Give me some information"
-          className="todo-input"
-          value = {this.state.value}
-          onChange={this.handleChangeVal}
-        />
-      </Form.Item>
-      <Button type="primary" onClick={this.addWork} className="submit-btn" disabled={!this.state.value}>Submit</Button>
-    </div>
-  );
+export default class InputForm extends React.Component {
+
+  handleChangeVal = (e) => {
+    this.props.onValueChange(e.target.value);
+  }
+
+  addWork = (e) => {
+    e.preventDefault();
+    this.props.onSubmitValue();
+  }
+
+  render(){
+    const value = this.props.value;
+
+    return (
+      <div>
+        <Form.Item>
+          <Input
+            placeholder="Give me some information"
+            className="todo-input"
+            value = {value}
+            onChange={this.handleChangeVal}
+          />
+        </Form.Item>
+        <Button type="primary" onClick={this.addWork} className="submit-btn" disabled={!value}>Submit</Button>
+      </div>
+    );
+  }
 }
--- src/WorkTable.js
+++ src/WorkTable.js
@@ -1,31 +1,60 @@
-import { Table, Space, Tag } from 'antd';
+import React from 'react';
+import { Button, Table, Space, Tag } from 'antd';
 const {Column} = Table;
 
-export default function WorkTable(props) {
-  return (
-    <div>
-      <Table dataSource={props.list} className="table">
-          <Column title="Work" dataIndex="work" key="work"/>
-          <Column title="Status" dataIndex="status" key="status"
-            render = {
-              (status) => (
-                <Tag color={this.colorStatus(status)} key={status}>
-                  {status.toUpperCase()}
-                </Tag>
-              )
-            }
-          />
-          <Column title="Action" key="action"
-            render = {(text, record, dataIndex) => (
-                <Space size="middle">
-                  <button onClick={this.setAsDone.bind(this, dataIndex)}>Set as Done</button>
-                  <button onClick={this.setInProgress.bind(this, dataIndex)}>Set as In Progress</button>
-                  <button onClick={this.deleteWork.bind(this, record.key)}>Delete</button>
-                </Space>
-              )
-            }
-          />
-      </Table>
-    </div>
-  );
+export default class WorkTable extends React.Component {
+
+  colorStatus = (status) => {
+    switch (status) {
+      case "done":
+        return "green";
+      case "in progress":
+        return "gold";
+      default:
+        return "cyan";
+    }
+  }
+
+  setAsDone = (index) => {
+    this.props.setDone(index);
+  }
+
+  setInProgress = (index) => {
+    this.props.setIP(index);
+  }
+
+  deleteWork = (key) => {
+    this.props.delete(key);
+  }
+
+  render(){
+
+  
+    return (
+      <div>
+        <Table dataSource={this.props.list} className="table">
+            <Column title="Work" dataIndex="work" key="work"/>
+            <Column title="Status" dataIndex="status" key="status"
+              render = {
+                (status) => (
+                  <Tag color={this.colorStatus(status)} key={status}>
+                    {status.toUpperCase()}
+                  </Tag>
+                )
+              }
+            />
+            <Column title="Action" key="action"
+              render = {(text, record, dataIndex) => (
+                  <Space size="middle">
+                    {/* <Button onClick={this.setAsDone(dataIndex)} className="submit-btn">Submit</Button>
+                    <Button onClick={this.setInProgress(dataIndex)} className="submit-btn">Submit</Button>
+                    <Button onClick={this.deleteWork(record.key)} className="submit-btn">Submit</Button> */}
+                  </Space>
+                )
+              }
+            />
+        </Table>
+      </div>
+    );
+  }
 }
